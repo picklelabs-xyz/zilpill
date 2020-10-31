77cbd4ba024423b623abad5e3cbb0021f580aae3
KA <asooja@gmail.com>
2020-10-31 13:20:15 +0000

    Add info email in swap.py for token prices.

--- src/exec/swap.py
+++ src/exec/swap.py
@@ -2,6 +2,8 @@ import src.utils.zil_utils as zutils
 import src.CONF as CONF
 import os
 import src.CONSTANTS as CNSTS
+from datetime import datetime
+from src.utils import email_info
 from src.arb.zil import zilswap
 from pyzil.account import Account
 from dotenv import load_dotenv
@@ -15,22 +17,43 @@ account = Account(private_key=zutils.get_key(os.getenv(CONF.SEC_WALLET['KEYSTORE
                                              os.getenv(CONF.SEC_WALLET['PASSWORD'])
                                              ))
 
+
 # Load zillion staking contract with your own wallet
 zilswap_contract = zutils.load_contract(CNSTS.CONTRACT.ZIL_SWAP_CONTRACT_ADD,
-                                              account)
+                                        account)
 
 pools = zilswap.get_contract_pools(zilswap_contract)
+
+now = datetime.now()
+dt = now.strftime("%d/%m/%Y %H:%M:%S")
 
 pool_token_count, pool_zil_count = zilswap.get_token_pool_size(
     pools,
     zutils.to_base16_add(CNSTS.TOKEN.GZIL_BECH32_ADD),
     zutils.get_token_dec_divisor(zutils.load_contract(CNSTS.TOKEN.GZIL_BECH32_ADD)))
 
-token_amount_to_be_sold = 10
 fees = zilswap.fees
 
-token_price_for_tx = zilswap.get_pool_token_sell_price(token_amount_to_be_sold,
-                                  pool_token_count,
-                                  pool_zil_count,
-                                  fees)
-print("Current GZIL price for ", token_amount_to_be_sold, " units: ", token_price_for_tx)
+token_amounts = [1, 10, 20]
+token_amount_and_price = {}
+infos = []
+
+for token_amount in token_amounts:
+    token_price_for_tx = zilswap.get_pool_token_sell_price(token_amount,
+                                                           pool_token_count,
+                                                           pool_zil_count,
+                                                           fees)
+    info = "GZIL price for " + str(token_amount) + " units: " + str(token_price_for_tx)
+    token_amount_and_price[token_amount] = token_price_for_tx
+    infos.append(info)
+
+e_subject = "swap: execution at " + dt
+e_from_name = "Machine"
+e_from_mail = os.getenv(CONF.EMAIL_1)
+e_from_pwd = os.getenv(CONF.EMAIL_1_PASSWORD)
+e_to_mails = [os.getenv(CONF.EMAIL_1)]
+e_msg = ""
+e_msg = e_msg + "\n\n".join(infos)
+print(e_msg)
+if token_amount_and_price[token_amounts[1]] > 1000:
+    email_info.mail_it(e_subject, e_msg, e_from_name, e_from_pwd, e_from_mail, e_to_mails)
