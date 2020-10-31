c7b34fa561e98460a08e19ca4f610daf399fa9d2
KA <asooja@gmail.com>
2020-10-31 16:20:43 +0000

    Add second email, and minor change to output.

--- src/exec/swap.py
+++ src/exec/swap.py
@@ -17,7 +17,6 @@ account = Account(private_key=zutils.get_key(os.getenv(CONF.SEC_WALLET['KEYSTORE
                                              os.getenv(CONF.SEC_WALLET['PASSWORD'])
                                              ))
 
-
 # Load zillion staking contract with your own wallet
 zilswap_contract = zutils.load_contract(CNSTS.CONTRACT.ZIL_SWAP_CONTRACT_ADD,
                                         account)
@@ -43,7 +42,8 @@ for token_amount in token_amounts:
                                                            pool_token_count,
                                                            pool_zil_count,
                                                            fees)
-    info = "GZIL price for " + str(token_amount) + " units: " + str(token_price_for_tx)
+    token_price_for_tx = round(token_price_for_tx)
+    info = "GZIL token price, if selling " + str(token_amount) + " units: " + str(token_price_for_tx)
     token_amount_and_price[token_amount] = token_price_for_tx
     infos.append(info)
 
@@ -51,9 +51,12 @@ e_subject = "swap: execution at " + dt
 e_from_name = "Machine"
 e_from_mail = os.getenv(CONF.EMAIL_1)
 e_from_pwd = os.getenv(CONF.EMAIL_1_PASSWORD)
-e_to_mails = [os.getenv(CONF.EMAIL_1)]
-e_msg = ""
-e_msg = e_msg + "\n\n".join(infos)
+e_to_mails = [os.getenv(CONF.EMAIL_1), os.getenv(CONF.EMAIL_2)]
+e_msg = "\n".join(infos)
+
+print("\n")
+print(e_subject)
 print(e_msg)
-if token_amount_and_price[token_amounts[1]] > 1000:
+
+if token_amount_and_price[token_amounts[1]] > 2500:
     email_info.mail_it(e_subject, e_msg, e_from_name, e_from_pwd, e_from_mail, e_to_mails)
