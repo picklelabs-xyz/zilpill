7c256f9fec2659e4f7b4a330985de58da67338df
KA <asooja@gmail.com>
2020-10-31 17:04:46 +0000

    Add more info in the email.

--- src/exec/withdraw_and_stake.py
+++ src/exec/withdraw_and_stake.py
@@ -34,7 +34,7 @@ now = datetime.now()
 dt = now.strftime("%d/%m/%Y %H:%M:%S")
 
 rewards, info = zillion.withdraw_all_stake_rewards(zillion_contract, zillion_proxy_contract,
-                                             ssn_adds, os.getenv(CONF.PRIM_WALLET['BECH32']))
+                                                   ssn_adds, os.getenv(CONF.PRIM_WALLET['BECH32']))
 print("Rewards: ", rewards)
 e_subject = "withdraw_and_stake: execution at " + dt
 
@@ -49,5 +49,9 @@ if rewards > 25:
     success = zillion.stake_zil(zillion_proxy_contract, CNSTS.SSN.SSN4_EZIL_BECH32, rewards)
     e_msg = e_msg + 'Staking: ' + str(success) + '\n\n'
 
+
 e_msg = e_msg + "\n".join(info)
+e_msg = e_msg + "\n"
+e_msg = e_msg + '\n' + 'Zillion: https://stake.zilliqa.com/address/' + os.getenv(CONF.PRIM_WALLET['BECH32'])
+e_msg = e_msg + '\n' + 'Viewblock: https://viewblock.io/zilliqa/address/' + os.getenv(CONF.PRIM_WALLET['BECH32'])
 email_info.mail_it(e_subject, e_msg, e_from_name, e_from_pwd, e_from_mail, e_to_mails)
